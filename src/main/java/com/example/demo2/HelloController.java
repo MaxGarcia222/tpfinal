0ac38bae6cb84983c1e3835bfbd410761c12c6cd
Maximo Manuel Garcia <134308971+MaxGarcia222@users.noreply.github.com>
2025-10-21 11:21:02 -0300

    Update HelloController.java

--- src/main/java/com/example/demo2/HelloController.java
+++ src/main/java/com/example/demo2/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("hola masi");
+        welcomeText.setText("luna gordita");
     }
 }
